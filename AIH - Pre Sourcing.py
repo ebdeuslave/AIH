1c0c899077eb22d0f46cbf9b718ec5b3c37b4d74
ebdeuslave <63257740+ebdeuslave@users.noreply.github.com>
2023-07-25 14:52:07 +0100

    Update AIH - Pre Sourcing.py

--- AIH - Pre Sourcing.py
+++ AIH - Pre Sourcing.py
@@ -11,10 +11,13 @@ import re, time, os, requests, sys, json
 from datetime import datetime, datetime, timedelta
 from dateutil.parser import parse
 from threading import Thread
-from __auth__ import * 
 from pathlib import Path
 import babel.numbers
 
+if not os.path.isfile("__auth__.py" or "suppliers.json"):
+    messagebox.showerror('AIH Sourcing', 'Missing File "__auth__" OR "suppliers.json"')
+    sys.exit()
+from __auth__ import * 
 
 ####################### Invoices Downloader & Products Exporter #######################
 fileN = f'{datetime.now().strftime("%Y-%m-%d-%Hh-%Mmin")}.csv'
@@ -25,61 +28,17 @@ with open("suppliers.json", "r") as f:
     ALL_PRODUCTS = [product for List in suppliers_products.values() for product in List]
 
 
-class Scraper:
-    def __init__(self,DictProducts = {DEFAULT_SUPPLIER: {}}, total_cmds = 0 ,total_products = 0, total_quantities = 0): 
+class Sourcing:
+    def __init__(self, DictProducts = {DEFAULT_SUPPLIER: {}}, total_cmds = 0, total_products = 0, total_quantities = 0): 
         self.DictProducts = DictProducts
         self.total_cmds = total_cmds
         self.total_products = total_products
         self.total_quantities = total_quantities
    
 
-    def Para_CMDs(self):
-        start = time.time()
-
-        if selected.get() == 'Parabio':
-            site = 'https://www.parabio.ma'
-            apiSite = 'https://www.parabio.ma/api/orders'
-        else:
-            site = f'https://{selected.get().lower()}.ma'
-            apiSite = f'https://{selected.get().lower()}.ma/api/orders'
-            
-        toDate_ = datetime.strptime(toDate.get(),'%Y-%m-%d')+timedelta(1)
-        toDate_ = toDate_.strftime('%Y-%m-%d')
-        exactTime_ = f'{exactTime.get()[:2]}:{exactTime.get()[2:4]}:{exactTime.get()[4:]}'
-        exactTime_ = re.sub('[^0-9:]', '', exactTime_)
-        ordersLink= f'{apiSite}?filter[invoice_date]=[{fromDate.get()} {exactTime_},{toDate_}]&filter[current_state]=[2,3]'
-        request = requests.get(ordersLink, auth=(apiKey,''))
-        root = etree.fromstring(request.content)
-        orders =  root.xpath('//order')
-        IDs = [order.attrib['id'] for order in orders]
-
-
-        for id in IDs:
-            cmdProgress.pack(pady=5)
-            cmdLabel.pack()
-            cmdLabel.config(text=f'Téléchargement : Commande {IDs.index(id)+1}/{len(IDs)}')
-            top.update_idletasks()
-            cmdProgress['value'] += 100/len(IDs)
-            pdf_link = f"{site}/generatePDF.php?id_order={id}&secure_key=U4ZRRXFIIS6Q27TZGDLSCJZ7M5RT1YGT"
-            pdf_request = requests.get(pdf_link)
-            filename = Path(f'pdf/{id}.pdf')
-            filename.write_bytes(pdf_request.content)
-
-
-        cmdProgress.stop()
-        cmdProgress.pack_forget()
-        cmdLabel.pack_forget()
-        end = time.time()
-        duration = end-start
-        messagebox.showinfo('Commandes', f'Terminée en: {round(duration,2)} Secs\n\n{len(IDs)} Commandes Téléchargées')
-        
-
-    def check_cmd(self):
-        cmdBtn.config(state='disabled')
-        From = fromDate.get()
-        From = re.sub('[^0-9-]' , '', From)
-        To = toDate.get()
-        To = re.sub('[^0-9-]' , '', To)
+    def __check_date_validation(self):
+        From = re.sub('[^0-9-]' , '', fromDate.get())
+        To = re.sub('[^0-9-]' , '', toDate.get())
         date1 = From.split('-')
         date2 = To.split('-')
         try:
@@ -87,20 +46,72 @@ class Scraper:
             parse(To)
             if date1 > date2:
                 messagebox.showerror('INVALID DATE', 'Date "From" must be before Date "To" - Set a VALID Date \nEx : \n2022-01-01 \n2022-01-02')
+                return 0
             else:
-                try: self.Para_CMDs()
-                except Exception as e:
+                return 1
+        except:
+            messagebox.showerror('INVALID DATE', 'Invalid Format - Set a VALID Date \nEx : \n2023-01-01 \n2023-01-02')
+            return 0
+
+
+
+
+    ########## Invoices downloader ##########
+    def Para_CMDs(self):
+        if not self.__check_date_validation(): return
+        start = time.time()
+
+        cmdBtn.config(state='disabled')
+
+        if selected.get() == 'Parabio':
+            apiSite = PARABIO_API_LINK
+        else:
+            apiSite = STORE_API_LINK
+            
+        toDate_ = datetime.strptime(toDate.get(),'%Y-%m-%d')+timedelta(1)
+        toDate_ = toDate_.strftime('%Y-%m-%d')
+        exactTime_ = re.sub('[^0-9]', '', exactTime.get())
+        exactTime_ = f'{exactTime_[:2]}:{exactTime_[2:4]}:{exactTime_[4:]}'
+        try:
+            ordersLink= f'{apiSite}?filter[invoice_date]=[{fromDate.get()} {exactTime_},{toDate_}]&filter[current_state]=[2,3]'
+            request = requests.get(ordersLink, auth=(apiKey,''))
+            root = etree.fromstring(request.content)
+            orders =  root.xpath('//order')
+            IDs = [order.attrib['id'] for order in orders]
+
+            if IDs:
+                for id in IDs:
+                    cmdProgress.pack(pady=5)
+                    cmdLabel.pack()
+                    cmdLabel.config(text=f'Téléchargement : Commande {IDs.index(id)+1}/{len(IDs)}')
+                    top.update_idletasks()
+                    cmdProgress['value'] += 100/len(IDs)
+                    pdf_link = PDF_LINK_GENERATOR
+                    pdf_request = requests.get(pdf_link)
+                    if not os.path.isdir("commandes"): os.mkdir("commandes")
+                    filename = Path(f'commandes/{id}.pdf')
+                    filename.write_bytes(pdf_request.content)
+
+
+                cmdProgress.stop()
+                cmdProgress.pack_forget()
+                cmdLabel.pack_forget()
+                end = time.time()
+                duration = end-start
+                messagebox.showinfo('Commandes', f'Terminée en: {round(duration,2)} Secs\n\n{len(IDs)} Commandes Téléchargées')
+            else:
+                messagebox.showinfo('Commandes' ,'9adia nashfa..')
+        
+        except Exception as e:
                     messagebox.showerror('Error', f'{e}\nerror line: {sys.exc_info()[-1].tb_lineno}')
                     cmdProgress.stop()
                     cmdLabel.config(text=f'Téléchargement : Erreur')
-        except:
-            messagebox.showerror('INVALID DATE', 'Invalid Format - Set a VALID Date \nEx : \n2022-01-01 \n2022-01-02')
-        
+                    
         cmdBtn.config(state='active')
+    
 
-
-
-    def getProducts(self, orderLink):
+    ########## Products Exporter ##########
+    def __getProducts(self, orderLink):
         try:
             r = requests.get(orderLink, auth=(apiKey,''))
             root = etree.fromstring(r.content)
@@ -141,29 +152,33 @@ class Scraper:
         dictInfo.config(bg="lightblue",text=f'Total CMDs : {self.total_cmds}\nTotal Produits : {self.total_products}\nTotal Quantité : {self.total_quantities}')
         exportBtn.config(state='disabled')
 
- 
+    
     def Para_Products(self):
+        if not self.__check_date_validation(): return
+
         start = time.time()
 
+        productsBtn.config(state='disabled')
         exportBtn.config(state='disabled')
+
         if selected.get() == "Parabio":
-            apiSite = f'https://www.{selected.get().lower()}.ma/api/orders' 
+            apiSite = PARABIO_API_URL
         else:
-            apiSite = f'https://{selected.get().lower()}.ma/api/orders'
+            apiSite = STORE_API_URL
         
-        exactTime_ = f'{exactTime.get()[:2]}:{exactTime.get()[2:4]}:{exactTime.get()[4:]}'
-        exactTime_ = re.sub('[^0-9:]', '', exactTime_)
         toDate_ = datetime.strptime(toDate.get(),'%Y-%m-%d')+timedelta(1)
         toDate_ = toDate_.strftime('%Y-%m-%d')
-        From = re.sub('[^0-9-]' , '', fromDate.get())
-        To = re.sub('[^0-9-]' , '', toDate_)
-        ordersLink= f'{apiSite}?filter[invoice_date]=[{From} {exactTime_},{To}]&filter[current_state]=[2,3]'
-        request = requests.get(ordersLink, auth=(apiKey,''))
-        root = etree.fromstring(request.content)
-        orders =  root.xpath('//order')
-        cmd_links = set([order.attrib['{http://www.w3.org/1999/xlink}href'] for order in orders])
-        
+        exactTime_ = re.sub('[^0-9]', '', exactTime.get())
+        exactTime_ = f'{exactTime_[:2]}:{exactTime_[2:4]}:{exactTime_[4:]}'
+
         try:
+            ordersLink= f'{apiSite}?filter[invoice_date]=[{fromDate.get()} {exactTime_},{toDate_}]&filter[current_state]=[2,3]'
+            request = requests.get(ordersLink, auth=(apiKey,''))
+            root = etree.fromstring(request.content)
+            orders =  root.xpath('//order')
+            cmd_links = set([order.attrib['{http://www.w3.org/1999/xlink}href'] for order in orders])
+        
+        
             for link in cmd_links:
                 progress.pack(pady=5)
                 productsLabel.pack()
@@ -171,7 +186,7 @@ class Scraper:
                 top.update_idletasks()
                 progress['value'] += 100/len(cmd_links)
                
-                self.getProducts(link)
+                self.__getProducts(link)
             
             self.total_cmds += len(cmd_links)
             progress.stop()
@@ -191,47 +206,28 @@ class Scraper:
             
 
         except Exception as e:
-            print(e,'\nerror line: ',sys.exc_info()[-1].tb_lineno)
-            messagebox.showerror('Produits', 'Zguel chi produit wla chi cmd 3awdo 7awlo')
+            messagebox.showerror('Error', f"{e},'\nerror line: ',{sys.exc_info()[-1].tb_lineno}")
             progress.stop()
             progress.pack_forget()
             productsLabel.pack_forget()
 
         if self.DictProducts != {"CDP": {}}: exportBtn.config(state='active')
 
-    
-    def check_products(self):
-        productsBtn.config(state='disabled')
-        From = fromDate.get()
-        From = re.sub('[^0-9-]' , '', From)
-        To = toDate.get()
-        To = re.sub('[^0-9-]' , '', To)
-        date1 = From.split('-')
-        date2 = To.split('-')
-        try:
-            parse(From)
-            parse(To)
-            if date1 > date2:
-                messagebox.showerror('INVALID DATE', 'Date "From" must be before Date "To" - Set a VALID Date \nEx : \n2022-01-01 \n2022-01-02')
-            else:
-                self.Para_Products()
-        except:
-            messagebox.showerror('INVALID DATE', 'Invalid Format - Set a VALID Date \nEx : \n2022-01-01 \n2022-01-02')
-
         productsBtn.config(state='active')
+    
 
-
+    ########## Suppliers Products Updater ##########
     def updateSuppliersProducts(self):
         global suppliers_products, ALL_PRODUCTS
         try:
-            r = requests.get("https://parapharma.ma/api/suppliers", auth=(apiKey,''))
+            r = requests.get(SUPPLIERS_LINK, auth=(apiKey,''))
             root = etree.fromstring(r.content)
             suppliers =  root.xpath("//supplier")
             EXCLUDED = ["3", "4", "10"]
             IDs = [supplier.attrib['id'] for supplier in suppliers if supplier.attrib['id'] not in EXCLUDED]
-            driver = webdriver.Chrome(ChromeDriverManager(version="114.0.5735.90").install())
+            driver = webdriver.Chrome(ChromeDriverManager().install())
             driver.maximize_window()
-            driver.get("https://parapharma.ma/admin112nza0id")
+            driver.get(PARAPHARMA_BACKOFFICE)
             user = driver.find_element(By.ID, "email")
             user.send_keys(u)
             passwd = driver.find_element(By.ID, "passwd")
@@ -247,7 +243,7 @@ class Scraper:
             token_value = driver.find_element(By.XPATH, '/html/body').get_attribute('data-token')
             suppliers_data = {}
             for id in IDs:
-                driver.get(f"https://parapharma.ma/admin112nza0id/index.php/sell/catalog/suppliers/{id}/products?_token={token_value}")
+                driver.get(f"https://parapharma.ma/ADMINISTRATION/index.php/sell/catalog/suppliers/{id}/products?_token={token_value}")
                 name = driver.find_element(By.TAG_NAME, "h1").text
                 products = driver.find_elements(By.CLASS_NAME, "card-header.clearfix")
                 suppliers_data[name] = [product.text for product in products]
@@ -261,18 +257,16 @@ class Scraper:
         
         
         except Exception as e:
-            print(e)
             messagebox.showerror('Fournisseurs', e)
 
 
-scraper = Scraper()
-
+autoSourcing = Sourcing()
 
 ################################ GUI ################################
 
 top = Tk()
-if os.path.isfile("para17.ico"):top.iconbitmap('para17.ico')
-top.title("AIH Sourcing - v3.0 BETA")
+if os.path.isfile("para17.ico"): top.iconbitmap('para17.ico')
+top.title("AIH Sourcing - v3.0")
 top.configure(background='coral')
 
 if os.path.isfile("bg.png"):
@@ -283,14 +277,14 @@ if os.path.isfile("bg.png"):
 menu = Menu()
 update = Menu(menu,tearoff=False)
 menu.add_cascade(label = 'Mise à jour', menu = update)
-update.add_command(label = 'Mettre à jour les fournisseurs' , command = scraper.updateSuppliersProducts)
+update.add_command(label = 'Mettre à jour les fournisseurs' , command = autoSourcing.updateSuppliersProducts)
 top.config(menu=menu)
 top.geometry('500x850+800+100')
 
-options = ['Parapharma', 'Coinpara', 'Parabio']
+stores = ['Parapharma', 'Coinpara', 'Parabio']
 selected = StringVar()
-selected.set( "Parapharma" )
-drop = OptionMenu( top , selected , *options )
+selected.set(stores[0])
+drop = OptionMenu( top , selected , *stores )
 drop.pack(pady=5)
 drop["menu"].configure(bg="lightgreen", borderwidth=1, font=("Helvetica", "9", 'bold'))
 
@@ -311,7 +305,7 @@ exactTime.insert(END, '000000')
 
 cmdFrame = LabelFrame(top,bd=5, text='Commandes', font=("Helvetica", "15", 'bold'), bg='lightblue')
 cmdFrame.pack(pady=20)
-cmdBtn = Button(cmdFrame,text='Télécharger',command=lambda: Thread(target=scraper.check_cmd).start(),activebackground='white',activeforeground='black',bg='cyan',bd =5,padx=5,pady=3,width=9,font='mincho 12')
+cmdBtn = Button(cmdFrame,text='Télécharger',command=lambda: Thread(target=autoSourcing.Para_CMDs).start(),activebackground='white',activeforeground='black',bg='cyan',bd =5,padx=5,pady=3,width=9,font='mincho 12')
 cmdBtn.pack(pady=20)
 cmdProgress = ttk.Progressbar(top, orient=HORIZONTAL, length=300, mode='determinate')
 cmdLabel = Label(top,bg='lightblue')
@@ -319,20 +313,16 @@ cmdLabel = Label(top,bg='lightblue')
 # Products Frame
 productsFrame = LabelFrame(top,bd=5, text='Produits', font=("Helvetica", "15", 'bold'), bg='#FCD1AC')
 productsFrame.pack()
-productsBtn = Button(productsFrame,text='Collecter',command=lambda: Thread(target=scraper.check_products).start(),activebackground='white',activeforeground='black',bg='LightPink1',bd =5,padx=5,pady=3,width=9,font='mincho 12')
+productsBtn = Button(productsFrame,text='Collecter',command=lambda: Thread(target=autoSourcing.Para_Products).start(),activebackground='white',activeforeground='black',bg='LightPink1',bd =5,padx=5,pady=3,width=9,font='mincho 12')
 productsBtn.pack(pady=20)
 
 
 dictInfo = Label(productsFrame, width=20, height=5, bg='lightblue',text=f'Total CMDs : 0\nTotal Produits : 0\nTotal Quantité : 0')
 dictInfo.pack(pady=20)
-exportBtn = Button(productsFrame, text='Export to CSV',bd =3, command=scraper.exportToCSV, state="disabled")
+exportBtn = Button(productsFrame, text='Export to CSV',bd =3, command=autoSourcing.exportToCSV, state="disabled")
 exportBtn.pack(pady=20)
 
 progress = ttk.Progressbar(top, orient=HORIZONTAL, length=300, mode='determinate')
 productsLabel = Label(top,bg='lightblue')
 
-# top.resizable(False, False)
-if os.path.isfile("__auth__.py" and "suppliers.json"):
-    top.mainloop()
-else:
-    messagebox.showerror('AIH Sourcing', 'Missing File "__auth__" OR "suppliers.json"')
+top.mainloop()
